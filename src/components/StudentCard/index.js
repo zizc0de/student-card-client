37a502a85766bca3b7c185f618f1dd1b05d2b991
Muhammad Abdul Aziz <zizcode.exporadev@gmail.com>
2018-06-23 07:11:21 +0700

    Added component StudentCard layout

---
+++ src/components/StudentCard/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './_style.scss';
+
+class StudentCard extends Component {
+	render() {
+		const { detail } = this.props;
+
+		return (
+			<div className="student-card">
+				<div className="student-card-body">
+					<div className="row">
+						<div className="col-12">
+							<div className="student-card__pict">
+								<img src={require('assets/images/user.png')} />
+							</div>
+							<div className="student-card__detail">
+								<p className="mb-0 text-semi-bold">{detail.name}</p>
+								<small>{detail.age} years old</small>
+							</div>
+						</div>
+					</div>
+					<div className="row mt-3">
+						<div className="col-12">
+							<div className="student-card__desc">
+								<p className="mb-0">{detail.bio}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="student-card-footer">
+					<Link to="/">Detail</Link>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default StudentCard;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,59 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import StudentCard from 'components/StudentCard';
+
 import './_style.scss';
 
 class Home extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			students: [
+				{
+					id: '1',
+					name: 'Muhammad Abdul Aziz',
+					age: '19',
+					bio: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+				},
+				{
+					id: '2',
+					name: 'Roro Kumala',
+					age: '20',
+					bio: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+				},
+				{
+					id: '3',
+					name: 'Hasta Ragil',
+					age: '21',
+					bio: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+				}							
+			]
+		}
+	};
+
 	render() {
+		const { students } = this.state;
+
 		return (
 			<div className="wrapper">
 				<div className="container">
-					<div className="row wrapper-content">
-						<div className="col-md-4">
-							adad
-						</div>
+					<div className="row">
+						<StudentList lists={students} />
 					</div>
 				</div>
 			</div>
 		);
 	}
 }
+
+const StudentList = ({ lists }) =>
+	<div style={{ display: 'flex', height: '100vh', width: '100%', alignItems: 'center', flexFlow: 'wrap' }} className="mt-3 mt-lg-0">
+		{lists.map((student) =>
+			<div key={student.id} className="col-lg-4 col-md-6 mb-3 mb-lg-0">
+				<StudentCard detail={student} />
+			</div>
+		)}
+	</div>
 
 export default Home;
